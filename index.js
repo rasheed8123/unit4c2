38aed34c09fd2e7f387c7702bcacd59cd901efbf
rasheed8123 <64723816+rasheed8123@users.noreply.github.com>
2022-03-14 21:53:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 
 const app = express();
+app.use(express.json());
 
 const connect = () =>{
     return mongoose.connect("mongodb://127.0.0.1:27017/unit4c2")
